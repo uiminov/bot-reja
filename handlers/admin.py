f8b46c7d5d14fd1120f929f47649b8d41123919d
uiminov <uiminovu@gmail.com>
2026-03-04 11:08:43 +0500

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,9 +3,11 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from handlers.admin import router as admin_router
 
 from config import ADMIN_ID
 import utils.db as db
+
 
 router = Router(name="admin")
 
