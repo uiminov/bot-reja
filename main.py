bbef6fc224cd5f14c27652fb9e2446f307d1f88d
uiminov <uiminovu@gmail.com>
2026-03-04 13:21:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-
-# Твои импорты
 from utils.db import init_db
 from config import BOT_TOKEN
 from handlers.subscription import router as subscription_router
@@ -11,28 +9,19 @@ from handlers.navigation import router as navigation_router
 from handlers.admin import router as admin_router
 
 async def main():
-    # 1. Инициализация базы
     init_db()
     
-    # 2. Настройка логирования (INFO поможет увидеть ошибки)
-    logging.basicConfig(level=logging.INFO)
+    # ИЗМЕНЕНО: ставим WARNING, чтобы не видеть INFO сообщения красным
+    logging.basicConfig(level=logging.WARNING)
 
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    # --- ВАЖНЫЙ ПОРЯДОК РЕГИСТРАЦИИ ---
-    # Мы ставим admin_router ПЕРВЫМ. 
-    # Теперь бот сначала проверит, не является ли сообщение командой /broadcast.
     dp.include_router(admin_router) 
-    
-    # Все остальные роутеры ниже
     dp.include_router(subscription_router)
     dp.include_router(payments_router)
     dp.include_router(navigation_router)
 
-    print("Бот запущен. Попробуй написать /broadcast в личку боту.")
-
-    # Очистка очереди и запуск
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
